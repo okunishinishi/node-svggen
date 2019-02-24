b232a5bf6cd1612425b0f7c4819118dd96755c89
okunishinishi <okunishinishi@gmail.com>
2019-02-24 23:59:27 +0900

    Version incremented to 3.0.1

--- test/writexml_test.js
+++ test/writexml_test.js
@@ -5,21 +5,20 @@
 'use strict'
 
 const writexml = require('../writexml')
-const co = require('co')
 const fs = require('fs')
 const assert = require('assert')
 
 describe('writexml', function () {
-  it('Get as string.', () => co(function * () {
+  it('Get as string.', async () => {
     let xml = writexml.string('baz', {
       foo: 'bar'
     })
     assert.ok(xml)
-  }))
+  })
 
-  it('Generate xml file.', () => co(function * () {
-    let filename = __dirname + '/tmp/foo/barbaz.txt'
-    yield writexml(filename, 'this', {
+  it('Generate xml file.', async () => {
+    let filename = __dirname + '/../tmp/foo/barbaz.xml'
+    await writexml(filename, 'this', {
       foo: 'bar'
     }, {
       mkdirp: true
@@ -28,7 +27,7 @@ describe('writexml', function () {
       fs.existsSync(filename)
     )
     assert.ok(fs.readFileSync(filename).toString())
-  }))
+  })
 })
 
 /* global describe, it */
--- writexml.js
+++ writexml.js
@@ -12,25 +12,22 @@
 
 const js2xml = require('js2xmlparser')
 const writeout = require('writeout')
-const co = require('co')
 const argx = require('argx')
 
 /** @lends writexml */
-function writexml (filename, elm, data, options = {}) {
+async function writexml(filename, elm, data, options = {}) {
   let args = argx(arguments)
   if (args.pop('function')) {
     throw new Error('Callback is no longer supported. Use promise interface instead.')
   }
   let xml = writexml.string(elm, data)
-  return co(function * () {
-    yield writeout(filename, xml, {
-      mkdirp: options.mkdirp
-    })
+  await writeout(filename, xml, {
+    mkdirp: options.mkdirp
   })
 }
 
 writexml.string = function (elm, data) {
-  return js2xml(elm || 'root', data, {
+  return js2xml.parse(elm || 'root', data, {
     useCDATA: true
   })
 }
